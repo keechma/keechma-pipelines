8c3e6fd15604b5dfa9def76b23c5f1f6ea46a315
Mihael Konjevic <konjevic@gmail.com>
2024-03-24 21:51:51 +0100

    New release

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject keechma/pipelines "0.1.2"
+(defproject keechma/pipelines "0.1.3"
   :description "Manager for async and concurrent code in ClojureScript"
   :url "https://github.com/keechma/keechma-pipelines"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :source-paths ["src"]
-  :dependencies [[funcool/promesa "5.1.0"]
+  :dependencies [[funcool/promesa "11.0.678"]
                  [medley "1.3.0"]
-                 [org.clojure/core.async "1.3.610"]])
+                 [org.clojure/core.async "1.6.681"]])
 
